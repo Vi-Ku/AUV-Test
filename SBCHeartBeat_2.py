32f0b3d3a412a53e2a086f97f7b1cbf0bf9b2cf3
Vishal Kumar <mpd18i027@iiitdm.ac.in>
2023-07-05 11:23:38 +0530

    Create SBCHeartBeat_2.py
    modified the board type as it was not working with the Default configuration.

---
+++ SBCHeartBeat_2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import rospy
+import RPi.GPIO as GPIO
+
+# Pin Definitions
+heartbeat_pin = 18  # BOARD pin 40
+heartBeatTime = 0.5
+
+def main():
+    # Init Node
+    rospy.init_node('heartbeat_node', anonymous=True)
+
+    # Pin Setup:
+    GPIO.setmode(GPIO.BCM)  # BCM pin-numbering scheme from Raspberry Pi
+    # set pin as an output pin with optional initial state of HIGH
+    GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
+    # Pin Setup:
+
+
+    rospy.loginfo("Starting heartbeat...")
+    try:
+        while not rospy.is_shutdown():
+            GPIO.output(heartbeat_pin, GPIO.HIGH)  # set the pin HIGH
+            rospy.sleep(heartBeatTime)  # wait half a second
+            GPIO.output(heartbeat_pin, GPIO.LOW)  # set the pin LOW
+            rospy.sleep(heartBeatTime)  # wait half a second
+    finally:
+        GPIO.cleanup()  # cleanup all GPIOs
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        GPIO.cleanup()  # cleanup all GPIOs
+        pass
